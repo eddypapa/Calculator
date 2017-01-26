53dadcf04fc61c740e977186a20ade066d63dea2
moressy <moressy@192.168.2.19>
2017-01-26 12:56:37 +0100

    Első Feltöltés

---
+++ calculator/calculator-api/src/main/java/hu/uni/miskolc/calculator/api/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.calculator.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ calculator/calculator-demo/src/main/java/hu/uni/miskolc/calculator/demo/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.calculator.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
